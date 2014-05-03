abc3b3c2848305db32b028e080a025dc877bf263
joshuaruesweg <josh@joshsboard.de>
2014-05-03 16:29:29 +0200

    add PackageServerUtil with user features for the auth.json

---
+++ files/lib/util/PackageServerUtil.class.php
@@ -0,0 +1,153 @@
+<?php
+namespace wcf\util; 
+
+use wcf\data\user\group\UserGroup; 
+use wcf\data\user\UserList; 
+use wcf\system\io\File; 
+use wcf\util\JSON; 
+use wcf\util\FileUtil; 
+
+/**
+ * Contains functions, which are related for "Tims-PackageServer".
+ * 
+ * @author		Joshua Rüsweg
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package		be.bastelstu.josh.ps
+ */
+final class PackageServerUtil {
+	
+	const AUTH_FILENAME = 'auth.json';
+	const GROUPID_PREFIX = 'group-';
+	const USERS_DIR = 'users';
+	const GROUPS_DIR = 'groups';
+	const PACKAGES_DIR = 'packages'; 
+	
+	/**
+	 * generates and save the auth.json-file
+	 */
+	public static function generateAuthFile() {
+		self::writeAuthFile(self::buildAuth());
+	}
+	
+	/**
+	 * writes an auth file with the given content
+	 * 
+	 * @param array $content
+	 */
+	public static function writeAuthFile(array $content) {
+		if (file_exists(self::getPackageServerPath().self::AUTH_FILENAME)) {
+			@unlink(self::getPackageServerPath().self::AUTH_FILENAME); 
+		}
+		
+		$file = new File(self::getPackageServerPath().self::AUTH_FILENAME); 
+		$file->write(JSON::encode($content)); 
+		$file->close();
+	}
+	
+	/**
+	 * returns the packageServer-dir with trailing slash
+	 * 
+	 * @return string
+	 */
+	public static function getPackageServerPath() {
+		return FileUtil::addTrailingSlash(PACKAGESERVER_DIR);
+	}
+	
+	/**
+	 * get the current auth file as array
+	 * if there isn't a auth file, the method returns null
+	 * 
+	 * @return array<mixed>
+	 */
+	public static function getAuthFileArray() {
+		if (file_exists(self::getPackageServerPath().self::AUTH_FILENAME)) {
+			return JSON::decode(file_get_contents(), true);
+		}
+		
+		return null; 
+	}
+	
+	/**
+	 * build a auth-file-array
+	 * 
+	 * @return array<mixed>
+	 */
+	public static function buildAuth() {
+		return array(
+		    self::USERS_DIR => self::buildUsersAuth(), 
+		    self::GROUPS_DIR => array(), 
+		    self::PACKAGES_DIR => array()
+		); 
+	}
+	
+	/**
+	 * build a array for the auth.json with all users
+	 */
+	public static function buildUsersAuth() {
+		$list = new UserList(); 
+		$list->readObjects(); 
+		
+		$users = array(); 
+		
+		foreach ($list->getObjects() as $user) {
+			$users[$user->username] = self::buildUserAuth($user); 
+		}  
+		
+		return $users; 
+	}
+	
+	/**
+	 * returns the user auth array for the auth.json
+	 * 
+	 * @param \wcf\util\wcf\data\user\User $user
+	 * @return array<mixed>
+	 */
+	public static function buildUserAuth(\wcf\data\user\User $user) {
+		return array(
+		    'passwd' => $user->password, 
+		    'groups' => self::getGroupIdentifersByIDs($user->getGroupIDs(true)), 
+		    'packages' => array(), // @TODO 
+		); 
+	}
+	
+	/**
+	 * updates the user in the auth.json
+	 * 
+	 * @param \wcf\data\user\User $user
+	 */
+	public static function updateUserAuth(\wcf\data\user\User $user) {
+		$auth = self::getAuthFileArray(); 
+		
+		$auth[self::USERS_DIR][$user->username] = self::buildUserAuth($user); // update user auth
+		
+		self::writeAuthFile($auth); 
+	}
+	
+	/**
+	 * returns a identifer for usergroups
+	 * 
+	 * @param \wcf\data\user\group\UserGroup $group
+	 * @return string
+	 */
+	public static function getGroupIdentifer(UserGroup $group) {
+		return self::GROUPID_PREFIX.$group->getObjectID(); 
+	}
+	
+	/**
+	 * returns group identifers for groups
+	 * 
+	 * @param array<integer> $groupIDs
+	 * @return array<string>
+	 */
+	public static function getGroupIdentifersByIDs(array $groupIDs) {
+		$groups = array(); 
+		
+		foreach ($groupIDs as $group) {
+			$groups[] = self::GROUPID_PREFIX.intval($group);
+		}
+		
+		return $groups; 
+	}
+	
+	private function __construct() { }
+}
