c94945171f511988b9f85b0000709b9f9dddf27e
joshuaruesweg <josh@joshsboard.de>
2014-05-15 15:40:25 +0200

    add table structure

---
+++ install.sql
@@ -0,0 +1,22 @@
+CREATE TABLE wcf1_packageserver_package_to_group (
+	packageIdentifier VARCHAR(255),
+	groupID INT(10),
+	permissions MEDIUMTEXT, 
+	PRIMARY KEY(packageIdentifier,groupID)
+);
+
+CREATE TABLE wcf1_packageserver_package_to_user (
+	packageIdentifier VARCHAR(255),
+	userID INT(10),
+	permissions MEDIUMTEXT, 
+	PRIMARY KEY(packageIdentifier,userID)
+);
+
+CREATE TABLE wcf1_packageserver_package_permission_general (
+	packageIdentifier VARCHAR(255) PRIMARY KEY,
+	permissions MEDIUMTEXT
+);
+
+-- foreign keys
+ALTER TABLE wcf1_packageserver_package_to_group ADD FOREIGN KEY (groupID) REFERENCES wcf1_user_group (groupID) ON DELETE CASCADE;
+ALTER TABLE wcf1_packageserver_package_to_user ADD FOREIGN KEY (userID) REFERENCES wcf1_user (userID) ON DELETE CASCADE;
