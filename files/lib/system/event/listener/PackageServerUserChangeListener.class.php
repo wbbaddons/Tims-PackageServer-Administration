fc7344d955ef710d5db192b4c570c83cc68a251d
Tim Düsterhus <tim@bastelstu.be>
2014-07-31 01:01:02 +0200

    Improve formatting and PHPDoc

--- files/lib/system/event/listener/PackageServerUserChangeListener.class.php
+++ files/lib/system/event/listener/PackageServerUserChangeListener.class.php
@@ -4,7 +4,7 @@ use wcf\system\event\IEventListener;
 use wcf\util\PackageServerUtil; 
 
 /**
- * Changes the auth.json-file for the PackageServer when a user is changed.
+ * Ipdates the authentication file of the PackageServer once a users’ information is updated.
  * 
  * @author		Joshua Rüsweg
  * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
@@ -19,35 +19,35 @@ class PackageServerUserChangeListener implements IEventListener {
 		if (!PACKAGESERVER_BUILDAUTH) return; 
 		
 		$parameters = $eventObj->getParameters(); 
-		
 		switch ($eventObj->getActionName()) {
 			case 'update':
 				// if the user or an administrator change the username
 				// we should replace the whole file so we make sure
 				// that the old username username is invalid
-				// we should change this in the future to an efficient method
+				// TODO: we should change this in the future to an efficient method
 				if (isset($parameters['data']['username'])) {
 					PackageServerUtil::generateAuthFile();
 				}
 				else if (isset($parameters['data']['removeGroups']) || isset($parameters['data']['password'])) {
+					// Update authentication information for the affected users.
 					foreach ($eventObj->getObjects() as $user) {
 						PackageServerUtil::updateUserAuth($user->getDecoratedObject());
 					}
 				}
-				break; 
+			break;
 				
 			case 'create': 
 			case 'addToGroups':
+				// Update authentication information for the affected users.
 				foreach ($eventObj->getObjects() as $user) {
 					PackageServerUtil::updateUserAuth($user->getDecoratedObject());
 				}
-				break; 
-				
+			break;	
 			case 'delete':
-				// generating a completely new file
-				// we should change this in the future
+				// Generating a completely new file
+				// TODO: We should improve this in the future
 				PackageServerUtil::generateAuthFile(); 
-				break;
+			break;
 		}
 	}
 
